c0a0629e698d32a67fdd8759fe969af6eb902e6a
Hansa26 <76100155+Hansa26@users.noreply.github.com>
2025-08-16 14:10:07 +0530

    JRA-0001: CRUD operations

--- basic_programs/second_fastapi_program/app.py
+++ basic_programs/second_fastapi_program/app.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Calculator(BaseModel):
+    a: int
+    b: int
+
 @app.get("/profile")
 def read_profile(name):
-    return {"message": f"Hello {name!r}, Welcome to Second FastAPI Program!"}
+    return {"message": f"Hello {name!r}, Welcome to Second FastAPI Program!, \nLet's introduce a simple calculator"}
+
+def add_numbers(a:int, b:int) -> int:
+    return a + b
+
+def sub_numbers(a:int, b:int) -> int:
+    return a - b
+
+@app.post("/add")
+def add_model(model: Calculator):
+    return add_numbers(model.a, model.b)
+
+@app.post("/sub")
+def sub_model(model: Calculator):
+    return sub_numbers(model.a, model.b)
